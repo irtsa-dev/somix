9bc7983953980e4b36d0c39c363b07d7c182783e
Irtsa <irtsa.development@gmail.com>
2025-08-28 17:39:20 -0500

    Update information.js

--- scripts/info/information.js
+++ scripts/info/information.js
@@ -1,1 +1,19 @@
+const effectsDiv = document.getElementsByClassName('effects')[0];
 
+
+
+
+for (const [key, value] of Objects.entries(Effects)) {
+    let ele = document.createElement('label');
+    ele.setAttribute('class', 'effectText');
+    ele.setAttribute('id', 'effect-' + key);
+    ele.setAttribute('style', 'color: rgb(' + value[1].join(', ') + ');');
+    ele.innerHTML = key;
+
+    let multLabel = document.getElementByID('effect-' + key);
+    ele = document.createElement('label')
+    ele.setAttribute('class', 'effectText');
+    ele.setAttribute('id', 'effect-' + key + '-mult');
+    ele.innerHTML = value[0];
+    multLabel.appendChild(ele);
+}
